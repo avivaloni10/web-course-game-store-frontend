3888478fdf9b7a93f02e3a34a90571f0115437be
Aviv Aloni <aviv@vulcan.io>
2023-01-16 01:53:16 +0200

    Add wishlist

--- src/components/common/products/index.js
+++ src/components/common/products/index.js
@@ -3,10 +3,24 @@ import {useEffect, useState} from "react";
 import {Container, Grid} from "@mui/material";
 
 import SingleProduct from "./SingleProduct";
+import {useAuth} from "../../../context/AuthContext";
+import {getOrCreateWishlist} from "../../../services";
 
 export default function Products({games, singleProductOverride}) {
+    const {getToken} = useAuth();
+
     const [showedGamesLimit, setShowedGamesLimit] = useState(12);
+    const [wishlist, setWishlist] = useState({games: []});
     const SingleProductView = singleProductOverride || SingleProduct
+
+    useEffect(() => {
+        async function fetchWishlist() {
+            const authToken = await getToken();
+            setWishlist(await getOrCreateWishlist(authToken));
+        }
+
+        fetchWishlist();
+    }, [getToken]);
 
     const renderProducts = () => {
         console.log(games);
@@ -14,7 +28,7 @@ export default function Products({games, singleProductOverride}) {
             return (
                 <Grid item key={product.id} xs={2} sm={4} md={4} display="flex" flexDirection={'column'}
                       alignItems="center">
-                    {<SingleProductView product={product}/>}
+                    {<SingleProductView product={product} wishlist={wishlist}/>}
                 </Grid>
             );
         }).slice(0, showedGamesLimit);
